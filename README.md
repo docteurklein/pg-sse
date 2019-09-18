1d74f3d4e3480718955068fa74d9fd5f8132fa1f
Klein Florian <florian.klein@free.fr>
2019-09-18 16:03:17 +0200

    use env to configure params

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 An http server sending postgres `LISTEN` payloads via Server-Sent-Events.
 
+
+## how ?
+
+    export PG_DSN=postgres://postgres@localhost
+    export BIND_ADDR=0.0.0.0:3000
+
+    cargo run
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,21 @@
 use hyper_sse::Server;
 use postgres::{Connection, TlsMode};
 use fallible_iterator::FallibleIterator;
+use std::env;
 
 lazy_static! {
     static ref SSE: Server<u8> = Server::new();
 }
 
 fn main() {
-    SSE.spawn("0.0.0.0:3000".parse().unwrap());
+    let bind_addr = env::var("BIND_ADDR").expect("BIND_ADDR");
+    let pg_dsn = env::var("PG_DSN").expect("PG_DSN");
+    SSE.spawn(bind_addr.parse().unwrap());
 
     let auth_token = SSE.generate_auth_token(Some(0)).unwrap();
-    println!("http://0.0.0.0:3000/push/0?{}", auth_token);
+    println!("http://{}/push/0?{}", bind_addr, auth_token);
 
-    let conn = Connection::connect("postgres://postgres@localhost", TlsMode::None).unwrap();
+    let conn = Connection::connect(pg_dsn, TlsMode::None).unwrap();
     conn.execute(&format!("LISTEN {}", &"test"), &[]).unwrap();
 
     let notifications = conn.notifications();
